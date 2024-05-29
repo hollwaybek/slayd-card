9226bb534911d8962fde4f95dc66e6776fafb84d
hollwaybek <hollwayuzb@gmail.com>
2024-05-29 20:54:44 +0500

    done

---
+++ main.js
@@ -0,0 +1,79 @@
+let arr = [
+    {
+        id: 1,
+        photo: 'https://avatars.dzeninfra.ru/get-zen_doc/1879615/pub_64d810be9e590127fb06215c_64d81237892c0461188d3afd/scale_1200',
+        title: 'BMW',
+        description: 'BMW m5 e60',
+        status: 'new',
+        cost: '22000$',
+    },
+    {
+        id: 2,
+        photo: 'https://avatars.dzeninfra.ru/get-zen_doc/1879615/pub_64d810be9e590127fb06215c_64d81237892c0461188d3afd/scale_1200',
+        title: 'BMW',
+        description: 'BMW m5 e60',
+        status: 'new',
+        cost: '22000$',
+    },
+    {
+        id: 3,
+        photo: 'https://avatars.dzeninfra.ru/get-zen_doc/1879615/pub_64d810be9e590127fb06215c_64d81237892c0461188d3afd/scale_1200',
+        title: 'BMW',
+        description: 'BMW m5 e60',
+        status: 'new',
+        cost: '22000$',
+    },
+    {
+        id: 4,
+        photo: 'https://avatars.dzeninfra.ru/get-zen_doc/1879615/pub_64d810be9e590127fb06215c_64d81237892c0461188d3afd/scale_1200',
+        title: 'BMW',
+        description: 'BMW m5 e60',
+        status: 'new',
+        cost: '22000$',
+    },
+    {
+        id: 5,
+        photo: 'https://avatars.dzeninfra.ru/get-zen_doc/1879615/pub_64d810be9e590127fb06215c_64d81237892c0461188d3afd/scale_1200',
+        title: 'BMW',
+        description: 'BMW m5 e60',
+        status: 'new',
+        cost: '22000$',
+    },
+    {
+        id: 6,
+        photo: 'https://avatars.dzeninfra.ru/get-zen_doc/1879615/pub_64d810be9e590127fb06215c_64d81237892c0461188d3afd/scale_1200',
+        title: 'BMW',
+        description: 'BMW m5 e60',
+        status: 'new',
+        cost: '22000$',
+    },
+    {
+        id: 7,
+        photo: 'https://avatars.dzeninfra.ru/get-zen_doc/1879615/pub_64d810be9e590127fb06215c_64d81237892c0461188d3afd/scale_1200',
+        title: 'BMW',
+        description: 'BMW m5 e60',
+        status: 'new',
+        cost: '22000$',
+    }, {
+        id: 8,
+        photo: 'https://avatars.dzeninfra.ru/get-zen_doc/1879615/pub_64d810be9e590127fb06215c_64d81237892c0461188d3afd/scale_1200',
+        title: 'BMW',
+        description: 'BMW m5 e60',
+        status: 'new',
+        cost: '22000$',
+    }, {
+        id: 9,
+        photo: 'https://avatars.dzeninfra.ru/get-zen_doc/1879615/pub_64d810be9e590127fb06215c_64d81237892c0461188d3afd/scale_1200',
+        title: 'BMW',
+        description: 'BMW m5 e60',
+        status: 'new',
+        cost: '22000$',
+    },
+]
+
+let mySwipper = document.querySelector('.mySwiper')
+const ReadCard = () => {
+    let.map((v) => {
+        
+    })
+}
